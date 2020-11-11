6ec929e4b050110091ee9280e55bfcdb523af2ee
rifaldidwi <rifaldi.dwi.d.w@gmail.com>
2020-11-11 22:26:45 +0700

    api route fix

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\AttendanceController;
+use App\Http\Controllers\API\ScheduleController;
 use App\Http\Controllers\API\StudentController;
+use App\Http\Controllers\API\SubjectController;
 use App\Http\Controllers\API\TeacherController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -29,21 +31,24 @@ Route::prefix('student')->group(function () {
     Route::post('{student}/logout', [StudentController::class, 'logout']);
 });
 
-Route::group(['middleware' => 'auth:sanctum'], function() {
-    Route::prefix('teacher/{teacher}')->group(function () {
-        Route::get('/', [TeacherController::class, 'show']);
-        Route::put('/', [TeacherController::class, 'update']);
-        Route::get('/schedules', [TeacherController::class, 'schedules']);
-        Route::get('/schedule/{schedule}/detail', [TeacherController::class, 'schedule']);
-        Route::post('/schedule/{schedule}/attendance', [TeacherController::class, 'schedule_attendance']);
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('subject', [SubjectController::class, 'index'])->name('all.subject');
+    Route::get('schedule/{schedule}', [ScheduleController::class, 'show'])->name('schedule.detail');
+    Route::post('schedule/{schedule}/attendance/{attendance}', [AttendanceController::class, 'store'])->name('schedule.attendance');
+    Route::get('attendance/{attendance}', [AttendanceController::class, 'show'])->name('attendance.detail');
+
+    Route::prefix('teacher')->group(function () {
+        Route::get('/{teacher}', [TeacherController::class, 'show'])->name('teacher.show.profile');
+        Route::put('/{teacher}', [TeacherController::class, 'update'])->name('teacher.edit.profile');
+        Route::get('/{teacher}/schedule', [ScheduleController::class, 'index'])->name('teacher.schedule');
+        Route::get('/{teacher}/attendance', [AttendanceController::class, 'index'])->name('teacher.attendance');
     });
-    
-    Route::prefix('student/{student}')->group(function () {
-        Route::get('/', [StudentController::class, 'show']);
-        Route::put('/', [StudentController::class, 'update']);
-        Route::get('/search_teacher', [StudentController::class, 'search']);
-        Route::post('/create_schedule', [StudentController::class, 'appointment']);
-        Route::get('/schedules', [StudentController::class, 'schedules']);
-        Route::get('/schedule/{schedule}/detail', [StudentController::class, 'schedule']);
+
+    Route::prefix('student')->group(function () {
+        Route::get('/{student}', [StudentController::class, 'show'])->name('student.show.profile');
+        Route::put('/{student}', [StudentController::class, 'update'])->name('student.edit.profile');
+        Route::get('/{student}/search/teacher', [TeacherController::class, 'search'])->name('search.teacher');
+        Route::get('/{student}/schedule', [ScheduleController::class, 'index'])->name('student.schedule');
+        Route::post('/{student}/schedule', [ScheduleController::class, 'index'])->name('add.schedule');
     });
 });
